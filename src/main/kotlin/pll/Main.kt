e69d430f15be059b3f06e02514c918114fd0e3f4
Doremi203 <maxim.r.ivanov@gmail.com>
2023-12-25 22:35:03 +0300

    Added Config. Finished registration

--- src/main/kotlin/pll/Main.kt
+++ src/main/kotlin/pll/Main.kt
@@ -3,6 +3,23 @@ package pll
 import di.Di
 
 fun main() {
+    runRegistration()
+    runApp()
+}
+
+private fun runRegistration() {
+    while (true) {
+        try {
+            Di.registrationMenu.show()
+            if (!Di.registrationMenu.processInputIfNotExit())
+                break
+        } catch (e: Exception) {
+            println("Произошла ошибка: ${e.message}")
+        }
+    }
+}
+
+private fun runApp() {
     while (true) {
         try {
             Di.registrationMenu.show()
